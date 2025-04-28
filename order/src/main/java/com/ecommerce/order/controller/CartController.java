bd581a4ff7d33a476a6bd34475ee37f4e1e14a1b
Harsh Gupta <hgupta7@umd.edu>
2025-04-28 17:25:50 -0400

    added configserver

--- order/src/main/java/com/ecommerce/order/controller/CartController.java
+++ order/src/main/java/com/ecommerce/order/controller/CartController.java
@@ -4,6 +4,7 @@ import com.ecommerce.order.dto.CartItemRequest;
 import com.ecommerce.order.model.CartItem;
 import com.ecommerce.order.service.CartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
