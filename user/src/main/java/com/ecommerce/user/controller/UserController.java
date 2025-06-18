54ac75819e808c0c1208ce731b5c5c2869fc5585
Harsh Gupta <hgupta7@umd.edu>
2025-06-17 23:56:55 -0400

    added observability, monitoring and distributed tracing

--- user/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -4,6 +4,9 @@ package com.ecommerce.user.controller;
 import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -26,6 +30,7 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id){
+        log.info("get user with id {}", id);
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
